8a7f8704bc3f89d622ea40e28ab342786928e2e9
DeOliveira07 <mr3863801@gmail.com>
2025-11-09 19:42:43 -0300

    dia 2

---
+++ aula10.py
@@ -0,0 +1,39 @@
+#inciando estruturas condicionais
+
+#tempo de vida de um carro
+
+# idade_carro = int(input('digite quantos anos tem o seu carro'))
+
+
+# if(idade_carro<=3):
+#     print('seu carro é novo')
+# else:
+#     print('seu carro não é novo')
+
+
+
+# nome_pesso = str(input('digite seu nome: '))
+# nome_pessoa_dif = str(input('digite o nome da outra pessoa: '))
+
+# if(nome_pesso == nome_pessoa_dif):
+#     print('estamos falando da mesma pessoa')
+
+
+# else:
+#     print(f'por {nome_pesso} ser diferente de {nome_pessoa_dif}, entramos no else ')
+
+# nota_um = float(input('digite a primeira nota : '))
+# nota_dois = float(input('digite a segunda nota : '))
+# media = (nota_um + nota_dois)/2
+# print(f'a sua média foi {media:.1f}')
+
+# if(media>=7):
+#     print('por isso você passou')
+
+# else:
+#     print('por isso você não passou')
+
+
+#hora dos desafios 
+
+#28
